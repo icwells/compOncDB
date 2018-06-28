74ed699af2ff795867dc903a65eb87394cf5b861
icwells <smrupp16@gmail.com>
2018-06-28 11:36:05 -0700

    Moved mapOfMaps and entryMap to own script

--- src/compOncDB/patients.go
+++ src/compOncDB/patients.go
@@ -90,35 +90,6 @@ func extractPatients(infile string, count int, tumor, acc map[string]map[string]
 	return entries
 }
 
-func mapOfMaps(t [][]string) map[string]map[string]string {
-	// Converts table to map of maps for easier searching
-	ret := make(map[string]map[string]string)
-	for _, row := range t {
-		if strarray.InMapMapStr(ret, row[1]) == true {
-			if strarray.InMapStr(ret[row[1]], row[2]) == false {
-				// Add to existing map
-				ret[row[1]][row[2]] = row[0]
-			}
-		} else {
-			// Make new sub-map
-			ret[row[1]] = make(map[string]string)
-			ret[row[1]][row[2]] = row[0]
-		}
-	}
-	return ret
-}
-
-func entryMap(t [][]string) map[string]string {
-	// Converts table to map for easier searching
-	m := make(map[string]string)
-	for _, i := range t {
-		if strarray.InMapStr(m, i[1]) == false {
-			m[i[1]] = i[0]
-		}
-	}
-	return m
-}
-
 func LoadPatients(db *sql.DB, col map[string]string, infile string) {
 	// Loads unique patient info to appropriate tables
 	m := dbIO.GetMax(db, "Patient", "ID")
