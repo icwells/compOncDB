5fb6bb5c7ed2a02703edad0d1772a76c8b786ef3
icwells <smrupp16@gmail.com>
2018-06-28 12:42:44 -0700

    Starting search script

---
+++ src/compOncDB/search.go
@@ -0,0 +1,16 @@
+// This script will search for given records from the comparative oncology database
+
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"dbIO"
+	"fmt"
+	"github.com/icwells/go-tools/iotools"
+	"github.com/icwells/go-tools/strarray"
+	"strconv"
+	"strings"
+)
+
+
