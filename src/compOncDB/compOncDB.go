4b3c0a9b8eb81fa37ff33debb299853ec87dc527
icwells <smrupp16@gmail.com>
2018-06-28 12:28:36 -0700

    Incoporated diagnosis upload and table dump

--- src/compOncDB/compOncDB.go
+++ src/compOncDB/compOncDB.go
@@ -55,14 +55,14 @@ func main() {
 		user      = kingpin.Flag("user", "MySQL username (default is root).").Short('u').Default("root").String()
 		ver       = kingpin.Flag("version", "Print version info and exit.").Short('v').Default("false").Bool()
 		bu        = kingpin.Flag("backup", "Backs up database to local machine.").Default("false").Bool()
-		New       = kingpin.Flag("new", "Initializes new tables in new database (database must be made manually).").Default("false").Bool()
+		New       = kingpin.Flag("new", "Initializes new tables in new database (database must be initialized manually).").Default("false").Bool()
 		taxa      = kingpin.Flag("taxa", "Load taxonomy tables from Kestrel output to update taxonomy and common name tables.").Default("false").Bool()
-		accounts  = kingpin.Flag("accounts", "Extract account info from input file and updates database.").Default("false").Bool()
+		accounts  = kingpin.Flag("accounts", "Extract account info from input file and update database.").Default("false").Bool()
+		diag      = kingpin.Flag("diagnosis", "Extract diagnosis info from input file and update database.").Default("false").Bool()
 		upload    = kingpin.Flag("upload", "Uploads patient info from input table to database.").Default("false").Bool()
-		//dump    = kingpin.Flag("dump", "Name of table to dump (writes all data from table to output file).").Short("d").PlaceHolder("nil").String()
-		infile = kingpin.Flag("infile", "Path to input file.").Short('i').PlaceHolder("nil").String()
-		//outfile = kingpin.Flag("outfile", "Name of output file.").Short('o').PlaceHolder("nil").String()
-		//cpu     = kingpin.Flag("t", "Number of threads (default = 1).").Default("1").Int()
+		dump      = kingpin.Flag("dump", "Name of table to dump (writes all data from table to output file).").Short('d').Default("nil").String()
+		infile    = kingpin.Flag("infile", "Path to input file.").Short('i').Default("nil").String()
+		outfile   = kingpin.Flag("outfile", "Name of output file.").Short('o').Default("nil").String()
 	)
 	kingpin.Parse()
 	if *ver == true {
@@ -80,20 +80,22 @@ func main() {
 		col := dbIO.ReadColumns(COL, false)
 		if *New == true {
 			dbIO.NewTables(db, COL)
-			/*} else if *dump != "nil" {
+		} else if *dump != "nil" {
 			// Extract entire table
 			if *outfile == "nil" {
 				fmt.Println("\n\t[Error] Please specify output file. Exiting.\n")
 				os.Exit(1)
 			}
 			table := dbIO.GetTable(db, *dump)
-			printCSV(*outfile, col[*dump], table)*/
+			printCSV(*outfile, col[*dump], table)
 		} else if *taxa == true {
 			// Upload taxonomy
 			LoadTaxa(db, col, *infile)
 		} else if *accounts == true {
 			// Upload account info
 			LoadAccounts(db, col, *infile)
+		} else if *diag == true {
+			LoadDiagnoses(db, col, *infile)
 		} else if *upload == true {
 			// Upload patient data
 			LoadPatients(db, col, *infile)
