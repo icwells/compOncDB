14d86d8b1b6fb62bc292ee39131fea529da50897
icwells <smrupp16@gmail.com>
2018-10-25 11:28:56 -0700

    Further streamlined install.sh and travis.yml

--- install.sh
+++ install.sh
@@ -10,12 +10,10 @@ DBI="dbIO"
 DR="github.com/go-sql-driver/mysql"
 IO="github.com/icwells/go-tools/iotools"
 KP="gopkg.in/alecthomas/kingpin.v2"
-LAN="golang.org/x/text/language"
 MAIN="compOncDB"
 PARSE="parseRecords"
 PR="github.com/Songmu/prompter"
 SA="github.com/icwells/go-tools/strarray"
-TS="golang.org/x/text/search"
 
 # Get install location
 SYS=$(ls $GOPATH/pkg | head -1)
@@ -27,7 +25,7 @@ echo "GOPATH identified as $GOPATH"
 echo ""
 
 # Get dependencies
-for I in $DR $IO $KP $LAN $PR $SA $TS; do
+for I in $DR $IO $KP $PR $SA; do
 	echo "Installing $I..."
 	go get -u $I
 	echo ""
