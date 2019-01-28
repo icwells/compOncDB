daf9c0f2b1c228503534a93ba4a3fe3934db3c48
Shawn Rupp <smrupp16@gmail.com>
2019-01-28 12:36:35 -0700

    Testing sizeOf

--- src/dbupload/patients.go
+++ src/dbupload/patients.go
@@ -20,15 +20,15 @@ func sizeOf(T interface{}) int {
 
 func getDenominator(size int) int {
 	// Returns denominator for subsetting upload slice (size in bytes / 16Mb)
-	p := float64(size)
 	max := 16777216.0
-	return int(math.Floor(p / max))
+	return int(math.Floor(float64(size) / max))
 }
 
 func uploadPatients(db *dbIO.DBIO, table string, list [][]string) {
 	// Uploads patient entries to db
 	l := len(list)
 	den := getDenominator(sizeOf(list))
+	fmt.Println(den, sizeOf(list))
 	if den <= 1 {
 		// Upload slice at once
 		vals, l := dbIO.FormatSlice(list)
