3002a7c55b8de3ad765148377fbd00ef6e3bae5f
Shawn Rupp <smrupp16@gmail.com>
2019-03-18 12:12:57 -0700

    Updated install script to accept input

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ DBI="github.com/icwells/dbIO"
 DBU="github.com/icwells/compOncDB/src/dbupload"
 DBE="github.com/icwells/compOncDB/src/dbextract"
 DR="github.com/go-sql-driver/mysql"
+FZ="github.com/lithammer/fuzzysearch/fuzzy"
 IO="github.com/icwells/go-tools/iotools"
 KP="gopkg.in/alecthomas/kingpin.v2"
 MAIN="compOncDB"
@@ -21,34 +22,58 @@ SA="github.com/icwells/go-tools/strarray"
 SYS=$(ls $GOPATH/pkg | head -1)
 PDIR=$GOPATH/pkg/$SYS
 
+installDependencies () {
+# Get dependencies
+	for I in $DR $FZ $IO $KP $PR $SA ; do
+		if [ ! -e "$PDIR/$I.a" ]; then
+			echo "Installing $I..."
+			go get -u $I
+			echo ""
+		fi
+	done
+}
+
+installDBIO () {
+	for I in $DBI $DBU $DBE; do
+		echo "Installing $I..."
+		go get -u $I
+		echo ""
+	done
+}
+
+installMain () {
+	# Install parseRecords
+	echo "Building $PARSE..."
+	go build -i -o bin/$PARSE src/$PARSE/*.go
+	echo ""
+
+	# compOncDB 
+	echo "Building main..."
+	go build -i -o bin/$MAIN src/$MAIN/*.go
+}
+
 echo ""
 echo "Preparing compOncDB package..."
 echo "GOPATH identified as $GOPATH"
 echo ""
 
-# Get dependencies
-for I in $DR $IO $KP $PR $SA ; do
-	if [ ! -e "$PDIR/$I.a" ]; then
-		echo "Installing $I..."
-		go get -u $I
-		echo ""
-	fi
-done
-
-for I in $DBI $DBU $DBE; do
-	echo "Installing $I..."
-	go get -u $I
+if [ $# -eq 0 ]; then
+	installMain
+elif [ $1 = "all" ]; then
+	installDependencies
+	installDBIO
+	installMain
+elif [ $1 = "db" ]; then
+	installDBIO
+	installMain
+elif [ $1 = "help" ]; then
+	echo "Installs Go scripts for compOnDB"
 	echo ""
-done
-
-# Intall parseRecords
-echo "Building $PARSE..."
-go build -o bin/$PARSE src/$PARSE/*.go
-echo ""
-
-# compOncDB 
-echo "Building main..."
-go build -o bin/$MAIN src/$MAIN/*.go
+	echo "all	Installs scripts and all depenencies."
+	echo "db	Installs scripts and dbIO only."
+	echo "help	Prints help text and exits."
+	echo ""
+fi
 
 echo "Finished"
 echo ""
