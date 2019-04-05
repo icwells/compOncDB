3402012c68eeb94aff1cedcc4d6bbfc6af2b9ad6
Shawn Rupp <smrupp16@gmail.com>
2019-04-05 16:31:53 -0700

    added testSetScores

--- src/parseRecords/accounts_test.go
+++ src/parseRecords/accounts_test.go
@@ -14,6 +14,57 @@ func getTestQueries() map[string][]string {
 	}
 }
 
+func getTestPool() map[string][]string {
+	// Returns map of expected pool
+	return map[string][]string{
+		"Payson Animal Hospital": {"Payson Animal Hospital", "Payson Animal Hospital", "Payson Animal Hospital"},
+		"Phoenix Veterinary Services": {"Phoenix Veterinary Services", "Phoenix Veterinary Services"},
+		"Phoenix Veterinarian Services": {"Phoenix Veterinarian Services"},
+	}
+}
+
+func getTestScores() map[string]int {
+	// Returns map of expected scores
+	return map[string]int{
+		"Payson Animal Hospital": 5,
+		"Phoenix Veterinary Services": 5,
+		"Phoenix Veterinarian Services": 4,
+	}
+}
+
+func getTestTerms() map[string]string {
+	// Returns map of expected terms
+	return map[string]string {
+		"payson animal hospital": "Payson Animal Hospital",
+		"Payson A. H.": "Payson Animal Hospital",
+		"Payson anim. Hospital": "Payson Animal Hospital",
+		"phoenix v.s.": "Phoenix Veterinary Services",
+		"phoenix v. s.": "Phoenix Veterinary Services",
+		"phoenix Veterinarian services": "Phoenix Veterinary Services",
+	}
+}
+
+func TestSetScores(t *testing.T) {
+	// Sets setScores (fuzzymatch and checkSpelling)
+	var s []string
+	expected := getTestScores()
+	a := newAccounts()
+	a.queries = getTestQueries()
+	a.pool = getTestPool()
+	for k := range a.pool {
+		s = append(s, k)
+	}
+	a.setScores(s)
+	for k, v := range expected {
+		a, ex := a.scores[k]
+		if ex == false {
+			t.Errorf("Expected key %s not present in scores map.", k)
+		} else if v != a {
+			t.Errorf("Actual score %d does not equal expected: %d.", a, v)
+		}
+	}	
+}
+
 func TestSetQueries(t *testing.T) {
 	// Tests setQueries (by extension: checkAmpersand, checkPeriods, and checkAbbreviations)
 	expected := getTestQueries()
@@ -37,9 +88,16 @@ func TestSetQueries(t *testing.T) {
 	}
 }
 
-func TestClusterNames(t *testing.T) {
+/*func TestClusterNames(t *testing.T) {
 	a := newAccounts()
 	a.queries = getTestQueries()
 	a.clusterNames()
 	t.Error(a.pool)
 }
+
+func TestSetTerms(t *testing.T) {
+	expected := getTestTerms()
+	a := newAccounts()
+	a.queries = getTestQueries()
+	a.pool = getTestPool()
+}*/
