9e7428fcc0944659247a66f4f20f2b0539b0710d
Shawn Rupp <smrupp16@gmail.com>
2019-07-22 12:57:20 -0700

    Added multiple search ability to search form. Need to add to go app.

--- codb/static/addFields.js
+++ codb/static/addFields.js
@@ -1,10 +1,9 @@
 // Contains functions for dynamically presenting logical search options 
 
-//var count = 0;
-let counter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let first = true;
+let COUNT = 0;
+let SEARCHES = {};
 
-const tables = {
+const TABLES = {
 	Patient: [["ID", "INT"], ["Sex", "TEXT"], ["Age", "DOUBLE"], ["Castrated", "TINYINT"], ["taxa_id", "INT"], ["source_id", "TEXT"], ["Date", "TEXT"], ["Comments", "TEXT"]],
 	Diagnosis: [["ID", "INT"], ["Masspresent", "TINYINT"], ["Hyperplasia", "TINYINT"], ["Necropsy", "TINYINT"], ["Metastasis", "TINYINT"]],
 	Tumor: [["ID", "INT"], ["primary_tumor", "TINYINT"], ["Malignant", "TINYINT"], ["Type", "TEXT"], ["Location", "TEXT"]],
@@ -18,37 +17,44 @@ const tables = {
 	Unmatched: [["sourceID", "TEXT"], ["name", "TEXT"], ["sex", "TEXT"], ["age", "DOUBLE"], ["date", "TEXT"], ["masspresent", "TINYINT"], ["necropsy", "TINYINT"], ["comments", "TEXT"], ["Service", "TEXT"]]
 };
 
-const template = (n) => `    <div id="searchField${n}">
-      <select id="Table${n}" name="Table${n}" onchange="addColumns('searchField${n}', this.id);">
-        <option name="Table${n}" value="Empty"></option>
+const ROW = (i, n) => `    <div id="parameter${n}">
+      <select id="${i}Table${n}" name="${i}Table${n}" onchange="SEARCHES[${i}].params[${n}].addColumns();">
+        <option name="${i}Table${n}" value="Empty"></option>
       </select>
-      <select id="Column${n}" name="Column${n}" onchange="addValue('searchField${n}', this.id);">
-        <option name="Column${n}" value="Empty"></option>
+      <select id="${i}Column${n}" name="${i}Column${n}" onchange="SEARCHES[${i}].params[${n}].addValue();">
+        <option name="${i}Column${n}" value="Empty"></option>
       </select>
-      <select id="Operator${n}" name="Operator${n}">
-        <option name="Operator${n}" value="=" selected="selected">=</option>
-        <option name="Operator${n}" value="!=">!=</option>
+      <select id="${i}Operator${n}" name="${i}Operator${n}">
+        <option name="${i}Operator${n}" value="=" selected="selected">=</option>
+        <option name="${i}Operator${n}" value="!=">!=</option>
       </select>
-      <input type="text" id="Value${n}" name="Value${n}">
-      <select id="Select${n}" name="Select${n}">
-        <option name="Select${n}" value="1">1</option>
-        <option name="Select${n}" value="0">0</option>
-        <option name="Select${n}" value="-1">-1</option>
+      <input type="text" id="${i}Value${n}" name="${i}Value${n}">
+      <select id="${i}Select${n}" name="${i}Select${n}">
+        <option name="${i}Select${n}" value="1">1</option>
+        <option name="${i}Select${n}" value="0">0</option>
+        <option name="${i}Select${n}" value="-1">-1</option>
       </select>
-      <input type="button" value="Remove" onClick="removeRow('searchField${n}');">
+      <input type="button" value="Remove" onClick="SEARCHES[${i}].removeRow('${n}');">
     </div>`;
 
-//----------------------------------------------------------------------------
+const SEARCH = (n) => `    <div id="search${n}">
+	</div>
+    <div id="options${n}">
+      <input type="button" value="Add search parameter" onClick="SEARCHES[${n}].addRow();">
+      <input type="button" value="Remove Search" onClick="removeSearch('${n}');">
+	<hr>
+    </div>`;
 
-class Session {
+//----------------------Parameter---------------------------------------------
+
+class Parameter {
 	// Stores values for single search field
-	constructor(id) {
-		// Isolate count and input field type 
-		this.count = id.slice(-1);
-		this.field = id.replace(this.count, "");
-		this.table = document.getElementById(`Table${this.count}`).value;
-		this.column = document.getElementById(`Column${this.count}`).value;
-		this.clearFields();
+	constructor(i, n) {
+		this.num = i;
+		this.count = n;
+		this.table = "Empty";
+		this.column = "Empty";
+		this.type = "TEXT";
 	}
 
 	clearSelect(id, end) {
@@ -60,126 +66,156 @@ class Session {
 		}
 	}
 
-	clearFields() {
+	clearFields(field) {
 		// Removes existing and subsequent select fields
-		if (this.field === "Table") {
-			this.clearSelect(`Column${this.count}`, 0);
-			this.clearSelect(`Operator${this.count}`, 1);
-		} else if (this.field === "Column") {
-			this.clearSelect(`Operator${this.count}`, 1);
+		if (field === "Table") {
+			this.clearSelect(`${this.num}Column${this.count}`, 0);
+			this.clearSelect(`${this.num}Operator${this.count}`, 1);
+		} else if (field === "Column") {
+			this.clearSelect(`${this.num}Operator${this.count}`, 1);
 		}
 	}
-}
 
-//-----------------------Values-----------------------------------------------
-
-function addOperations(n) {
-	// Adds values to dropdown
-	let ops = [">=", "<=", ">", "<"];
-	ops.forEach(o => {
-		let opt = document.createElement("option");
-		opt.text = o;
-		document.getElementById(`Operator${n}`).add(opt);
-	});
-}
-
-function toggleInputs(type, n) {
-	// Switches between select/text input
-	let value = document.getElementById(`Value${n}`);
-	let select = document.getElementById(`Select${n}`);
-	if (type === "TINYINT") {
-		// Reveal select block
-		select.style.display = "inline";
-		value.style.display = "none";
-	} else {
-		// Reveal text input
-		select.style.display = "none";
-		value.style.display = "inline";
-		value.type = "text";
+	addOperations() {
+		// Adds values to dropdown
+		let ops = [">=", "<=", ">", "<"];
+		ops.forEach(o => {
+			let opt = document.createElement("option");
+			opt.text = o;
+			document.getElementById(`${this.num}Operator${this.count}`).add(opt);
+		});
 	}
-}
 
-function getDataType(table, column) {
-	// Returns data type of given column value
-	let t = tables[table];
-	for (let i in t) {
-		if (t[i][0] === column) {
-			return t[i][1];
-		} 
+	toggleInputs() {
+		// Switches between select/text input
+		let value = document.getElementById(`${this.num}Value${this.count}`);
+		let select = document.getElementById(`${this.num}Select${this.count}`);
+		if (this.type === "TINYINT") {
+			// Reveal select block
+			select.style.display = "inline";
+			value.style.display = "none";
+		} else {
+			// Reveal text input
+			select.style.display = "none";
+			value.style.display = "inline";
+			value.type = "text";
+		}
 	}
-	return null;
-}
 
-function addValue(divname, id) {
-	// Adds operation and value inputs
-	let s = new Session(id);
-	let type = getDataType(s.table, s.column);
-	if (type != null) {
-		let val = `Value${s.count}`;
-		let sel = `Select${s.count}`;
-		toggleInputs(type, s.count);
-		if (type === "INT" || type === "DOUBLE") {
-			// Format number input for decimal/integer
-			addOperations(s.count);
-			document.getElementById(val).type = "number";
-			document.getElementById(val).min = "0";
-			if (type === "DOUBLE") {
-				// Add step
-				document.getElementById(val).step = "0.01";
+	getDataType() {
+		// Returns data type of given column value
+		let t = TABLES[this.table];
+		for (let i in t) {
+			if (t[i][0] === this.column) {
+				this.type = t[i][1];
+				break;
+			} 
+		}
+	}
+
+	addValue() {
+		// Adds operation and value inputs
+		this.clearFields("Column");
+		this.column = document.getElementById(`${this.num}Column${this.count}`).value;
+		if (this.column != "Empty") {
+			this.getDataType();
+			this.toggleInputs();
+			if (this.type === "INT" || this.type === "DOUBLE") {
+				// Format number input for decimal/integer
+				this.addOperations();
+				let elem = document.getElementById(`${this.num}Value${this.count}`);
+				elem.type = "number";
+				elem.min = "0";
+				if (this.type === "DOUBLE") {
+					// Add step
+					elem.step = "0.01";
+				}
 			}
 		}
 	}
-}
 
-function addColumns(divname, id) {
-	// Adds selector for columns from given table
-	let s = new Session(id);
-	if (document.getElementById(id).value != "Empty") {
-		if (s.table) {
-			tables[s.table].forEach(name => {
+	addColumns() {
+		// Adds selector for columns from given table
+		this.clearFields("Table");
+		this.table = document.getElementById(`${this.num}Table${this.count}`).value;
+		if (this.table != "Empty") {
+			TABLES[this.table].forEach(name => {
 				let opt = document.createElement("option");
 				opt.text = name[0];
-				document.getElementById(`Column${s.count}`).add(opt);
-			});
-		}		
+				document.getElementById(`${this.num}Column${this.count}`).add(opt);
+			});		
+		}
+	}
+
+	getTables() {
+		// Formats select field for tables
+		let tableselect = document.getElementById(`${this.num}Table${this.count}`);
+		let tablenames = Object.keys(TABLES);
+		tablenames.forEach(name => {
+			let opt = document.createElement("option");
+			opt.text = name;
+			tableselect.add(opt);
+		});
 	}
 }
 
-//--------------------------NewRow--------------------------------------------
+//--------------------------Search--------------------------------------------
 
-function removeRow(divname) {
-	// Removes row and adds number back to array
-	let n = divname.slice(-1);
-	let child = document.getElementById(divname);
-	let parent = child.parentNode;
-	parent.removeChild(child);
-	counter.unshift(n);
+class Search {
+	constructor(n) {
+		this.num = n;
+		this.count = 0;
+		this.params = {};
+	}
+
+	removeRow(n) {
+		// Removes row from document and map
+		let child = document.getElementById(`parameter${n}`);
+		let parent = child.parentNode;
+		parent.removeChild(child);
+		delete this.params[n];
+	}
+
+	addRow() {
+		// Adds new row to table search
+		if (this.count < 10) {
+			// Create new div
+			let newdiv = document.createElement("div");
+			newdiv.innerHTML = ROW(this.num, this.count);
+			document.getElementById(`search${this.num}`).appendChild(newdiv);
+			// Get new object, populate tables list, hide value select, and add to map
+			let p = new Parameter(this.num, this.count);
+			p.getTables();
+			p.toggleInputs();
+			this.params[this.count] = p;
+			this.count++;
+		}
+	}
 }
 
-function getTables(n) {
-	// Formats select field for tables
-	let tableselect = document.getElementById(`Table${n}`);
-	let tablenames = Object.keys(tables);
-	tablenames.forEach(name => {
-		let opt = document.createElement("option");
-		opt.text = name;
-		tableselect.add(opt);
-	});
-}
+//--------------------------Functions-----------------------------------------
 
-function addRow() {
-	// Adds new row to table search
-	if (counter.length > 0) {
-		// Limit to ten search fields
-		let n = counter[0];
-		counter.shift();
+function newSearch(divname) {
+	// Adds new search to form
+	if (COUNT < 10) {
 		// Create new div
 		let newdiv = document.createElement("div");
-		newdiv.innerHTML = template(n);
-		document.getElementById("Search").appendChild(newdiv);
-		// Populate tables list
-		getTables(n);
-		// Hide value select
-		toggleInputs("TEXT", n);
+		newdiv.innerHTML = SEARCH(COUNT);
+		document.getElementById("SearchBlock").appendChild(newdiv);
+		// Create new search object
+		let s = new Search(COUNT);
+		s.addRow();
+		SEARCHES[COUNT] = s;
+		COUNT++;
 	}
+}
+
+function removeSearch(n) {
+	// Removes search from document and map
+	let child = document.getElementById(`search${n}`);
+	let opt = document.getElementById(`options${n}`);
+	let parent = child.parentNode;
+	parent.removeChild(child);
+	parent.removeChild(opt);
+	delete SEARCHES[n];
 }
