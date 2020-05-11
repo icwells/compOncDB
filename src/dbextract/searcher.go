07087faeaa3c3c0091d0df5501d5e86517fd3d3b
Shawn Rupp <smrupp16@gmail.com>
2020-05-11 12:46:33 -0700

    Adding Year column

--- src/dbextract/searcher.go
+++ src/dbextract/searcher.go
@@ -29,7 +29,7 @@ func newSearcher(db *dbIO.DBIO, inf bool) *searcher {
 	s.res = make(map[string][]string)
 	s.taxa = make(map[string][]string)
 	// Add default header
-	s.header = "ID,Sex,Age,Castrated,taxa_id,source_id,source_name,Date,Comments,"
+	s.header = "ID,Sex,Age,Castrated,taxa_id,source_id,source_name,Date,Year,Comments,"
 	s.header = s.header + "Masspresent,Hyperplasia,Necropsy,Metastasis,primary_tumor,Malignant,Type,Location,"
 	s.header = s.header + "Kingdom,Phylum,Class,Orders,Family,Genus,Species,service_name,Zoo,Aza,Institute,account_id"
 	s.db = db
