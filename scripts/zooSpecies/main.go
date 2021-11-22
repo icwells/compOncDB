fd3e1df87507be773f7e5a36eadf08cf98f571ff
icwells <smrupp16@gmail.com>
2021-11-22 13:21:27 -0700

    Updated zooSpecies

--- scripts/zooSpecies/main.go
+++ scripts/zooSpecies/main.go
@@ -5,10 +5,11 @@ package main
 import (
 	"fmt"
 	"github.com/icwells/compOncDB/src/codbutils"
-	"github.com/icwells/go-tools/iotools"
 	"github.com/icwells/dbIO"
+	"github.com/icwells/go-tools/iotools"
 	"github.com/icwells/simpleset"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"sort"
 	"strings"
 	"time"
 )
@@ -17,19 +18,16 @@ var (
 	approved = kingpin.Flag("approved", "Extract only approved sources.").Default("false").Bool()
 	infile   = kingpin.Flag("infile", "Name of input csv.").Short('i').Required().String()
 	outfile  = kingpin.Flag("outfile", "Name of output file.").Short('o').Required().String()
-	user     = kingpin.Flag("user", "MySQL username (default is root).").Short('u').Required().String()
+	user     = kingpin.Flag("user", "MySQL username.").Short('u').Required().String()
 )
 
 type zoos struct {
 	accounts map[string]string
-	all		*simpleset.Set
-	db		*dbIO.DBIO
-	header  map[string]int
-	ids		map[string][]string
-	records [][]string
-	sources map[string][]string
-	species map[string]*simpleset.Set
-	taxa	map[string]*simpleset.Set
+	all      *simpleset.Set
+	db       *dbIO.DBIO
+	ids      map[string][]string
+	sources  map[string]string
+	taxa     map[string]*simpleset.Set
 }
 
 func newZoos() *zoos {
@@ -38,47 +36,31 @@ func newZoos() *zoos {
 	z.db = codbutils.ConnectToDatabase(codbutils.SetConfiguration(*user, false), "")
 	z.accounts = codbutils.EntryMap(z.db.GetColumns("Accounts", []string{"submitter_name", "account_id"}))
 	z.all = simpleset.NewStringSet()
-	z.records, z.header = iotools.ReadFile(*infile, true)
-	z.ids = codbutils.ToMap(z.db.GetColumns("Patient", []string{"taxa_id", "ID"}))
-	z.sources = codbutils.ToMap(z.db.GetColumns("Source", []string{"ID", "Zoo", "Institute", "Approved", "account_id"}))
-	z.species = make(map[string]*simpleset.Set)
+	z.sources = codbutils.EntryMap(z.db.GetColumns("Source", []string{"account_id", "ID"}))
 	z.taxa = make(map[string]*simpleset.Set)
 	return z
 }
 
-func (z *zoos) setTaxa() {
+func (z *zoos) setAccounts() {
 	// Stores patient ids by taxa
 	fmt.Println("\n\tStoring patient IDs by species...")
-	taxa := codbutils.EntryMap(z.db.GetColumns("Taxonomy", []string{"taxa_id", "Species"}))
-	for _, i := range z.records {
-		sp := i[z.header["Species"]]
-		if _, ex := z.taxa[sp]; !ex {
-			z.taxa[sp] = simpleset.NewStringSet()
-		}
-		if tid, ex := taxa[sp]; ex {
-			if pids, ex := z.ids[tid]; ex {
-				for _, p := range pids {
-					z.taxa[sp].Add(p)
-				}
-			}
-		}
+	var species []string
+	records, header := iotools.ReadFile(*infile, true)
+	for _, i := range records {
+		species = append(species, i[header["Species"]])
 	}
-}
-
-func (z *zoos) setNames() {
-	// Stores zoo names for each species
-	fmt.Println("\tStoring account names by species...")
-	for k, v := range z.taxa {
-		z.species[k] = simpleset.NewStringSet()
-		for _, i := range v.ToStringSlice() {
-			if s, ex := z.sources[i]; ex {
-				if s[0] == "1" || s[1] == "1" {
-					// Ignore private records
-					if !*approved || s[2] == "1" {
-						if name, ex := z.accounts[s[3]]; ex {
-							z.species[k].Add(name)
-							z.all.Add(name)
+	for _, i := range z.db.GetRows("Records", "Species", strings.Join(species, ","), "Species,Zoo,Institute,Approved,ID") {
+		if !*approved || i[3] == "1" {
+			if i[1] == "1" || i[2] == "1" {
+				// Select approved zoo/institute records
+				sp := i[0]
+				if aid, exists := z.sources[i[4]]; exists {
+					if v, ex := z.accounts[aid]; ex {
+						if _, ex := z.taxa[sp]; !ex {
+							z.taxa[sp] = simpleset.NewStringSet()
 						}
+						z.taxa[sp].Add(v)
+						z.all.Add(v)
 					}
 				}
 			}
@@ -86,20 +68,23 @@ func (z *zoos) setNames() {
 	}
 }
 
+func (z *zoos) sortAccounts(s *simpleset.Set) string {
+	// Converts set to string slice, sorts slice, and returns joined string
+	ret := s.ToStringSlice()
+	sort.Strings(ret)
+	return strings.Join(ret, ";")
+}
+
 func (z *zoos) write() {
 	// Writes names to file
 	out := iotools.CreateFile(*outfile)
-	out.WriteString("Genus,Species,Necropsies,Zoos\n")
-	all := append([]string{"", "all", ""}, strings.Join(z.all.ToStringSlice(), ";"))
+	out.WriteString("Genus,Species,Zoos\n")
+	all := []string{"", "all", z.sortAccounts(z.all)}
 	out.WriteString(strings.Join(all, ",") + "\n")
-	for _, i := range z.records {
-		sp := i[z.header["Species"]]
-		if v, ex := z.species[sp]; ex {
-			genus := i[z.header["Genus"]]
-			nec := i[z.header["Necropsies"]]
-			row := append([]string{genus, sp, nec}, strings.Join(v.ToStringSlice(), ";"))
-			out.WriteString(strings.Join(row, ",") + "\n")
-		}
+	for k, v := range z.taxa {
+		genus := strings.Split(k, " ")[0]
+		row := []string{genus, k, z.sortAccounts(v)}
+		out.WriteString(strings.Join(row, ",") + "\n")
 	}
 }
 
@@ -107,8 +92,7 @@ func main() {
 	start := time.Now()
 	kingpin.Parse()
 	z := newZoos()
-	z.setTaxa()
-	z.setNames()
+	z.setAccounts()
 	z.write()
 	fmt.Printf("\tFinished. Runtime: %s\n\n", time.Since(start))
 }
