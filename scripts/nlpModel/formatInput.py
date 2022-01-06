a694e3f034e9b36873607796f1122860b50a844d
icwells <smrupp16@gmail.com>
2022-01-06 16:05:33 -0700

    Split location and type into seperate outputs

--- scripts/nlpModel/formatInput.py
+++ scripts/nlpModel/formatInput.py
@@ -1,63 +1,100 @@
 '''Preformats input file for nlpModeler.'''
 
+from random import shuffle
 import re
 from unixpath import readFile
 
 class Formatter():
 
 	def __init__(self, infile, outfile, encodingfile):
-		self.count = 1
+		self.cancer = []
 		self.encodingfile = encodingfile
 		self.header = {}
 		self.infile = infile
+		self.lcount = 1
+		self.locations = {"NA": 0}
+		self.nas = []
+		self.noncancer = []
 		self.outfile = outfile
-		self.types = {"NA-NA": 0}
+		self.tcount = 1
+		self.total = 0
+		self.types = {"NA": 0}
 		self.__formatFile__()
+		self.__writeLists__()
 		self.__writeDicts__()
 
 	def __writeDicts__(self):
 		# Writes locations and types to file
 		with open(self.encodingfile, "w") as out:
+			for k in self.locations.keys():
+				out.write("Location,{},{}\n".format(k, self.locations[k]))
 			for k in self.types.keys():
-				out.write("{},{}\n".format(k, self.types[k]))
+				out.write("Type,{},{}\n".format(k, self.types[k]))
+
+	def __formatOutput__(self):
+		# Returns randomly sorted list, truncates records with na for comments
+		shuffle(self.nas)
+		ret = self.cancer
+		ret.extend(self.noncancer)
+		#Sret.extend(self.nas[:int(len(ret) * 0.05)])
+		shuffle(ret)
+		print("\tFormatted {} of {} records.".format(len(ret), self.total))
+		return ret
+
+	def __writeLists__(self):
+		# Writes data to file
+		with open(self.outfile, "w") as out:
+			header = []
+			for i in range(len(self.header)):
+				header.append(-1)
+			for k in self.header.keys():
+				header[self.header[k]] = k
+			# Omit Location column
+			out.write(",".join(header) + "\n")
+			for i in self.__formatOutput__():
+				out.write(",".join(i) + "\n")
 
 	def __formatLine__(self, line):
 		# Replaces punctuation, splits compound locations and types, and encodes paired locations and types as integers
 		rows = []
+		go = True
 		if line[self.header["Comments"]] == "NA" or line[self.header["Comments"]] == "n/a. n/a.":
-			if line[self.header["Masspresent"]] == "1":
+			go = False
+			if line[self.header["Masspresent"]] != "1":
 				# Skip records where diagnosis info is not in comments
-				return rows
-		line[self.header["Comments"]] = re.sub(r"[^\w\s]", "", line[self.header["Comments"]])
-		# Split compound locations and types
-		loc = line[self.header["Location"]].split(";")
-		for idx, i in enumerate(line[self.header["Type"]].split(";")):
-			# Combine location and type as one key
-			t = "-".join([loc[idx], i])
-			if t not in self.types.keys():
-				self.types[t] = self.count
-				self.count += 1
-			row = line[:self.header["Type"]]
-			row.append(str(self.types[t]))
-			rows.append(row)
-		return rows
+				line[-1] = "0"
+				line[-2] = "0"
+				self.nas.append(line)
+		if go:
+			line[self.header["Comments"]] = re.sub(r"[^\w\s]", "", line[self.header["Comments"]])
+			# Split compound locations and types; store only one for now
+			loc = line[self.header["Location"]].split(";")
+			for idx, i in enumerate(line[self.header["Type"]].split(";")):
+				# Combine location and type as one key
+				l = loc[idx]
+				if l not in self.locations.keys():
+					self.locations[l] = self.lcount
+					self.lcount += 1
+				if i not in self.types.keys():
+					self.types[i] = self.tcount
+					self.tcount += 1
+				row = line[:self.header["Type"]]
+				row.append(str(self.types[i]))
+				row.append(str(self.locations[l]))
+				rows.append(row)
+			if line[self.header["Masspresent"]] == "1":
+				self.cancer.append(row)
+			else:
+				self.noncancer.append(row)
 
 	def __formatFile__(self):
 		# Reads input file, formats values, and writes to output
 		first = True
 		print("\n\tFormatting input file...")
-		with open(self.outfile, "w") as out:
-			for line in readFile(self.infile, header = True, d = ","):
-				if not first:
-					for i in self.__formatLine__(line):
-						out.write(",".join(i) + "\n")
-				else:
-					self.header = line
-					row = []
-					for i in range(len(self.header)):
-						row.append(-1)
-					for k in self.header.keys():
-						row[self.header[k]] = k
-					# Omit Location column
-					out.write(",".join(row[:-1]) + "\n")
-					first = False
+		for line in readFile(self.infile, header = True, d = ","):
+			if not first:
+				self.total += 1
+				self.__formatLine__(line)
+			else:
+				self.header = line
+				first = False
--- scripts/nlpModel/nlpModel.py
+++ scripts/nlpModel/nlpModel.py
@@ -1,4 +1,4 @@
-'''Defines TensorFlow model for common/scientifc name classifier'''
+'''Defines TensorFlow model for comparative oncology record diagnosis.'''
 
 from argparse import ArgumentParser
 from datetime import datetime
@@ -10,7 +10,6 @@ from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-import tensorflow_hub as hub
 from unixpath import readFile
 
 INFILE = "diagnoses.csv"
@@ -19,14 +18,11 @@ ENCODING = "typeEncodings.csv"
 class Classifier():
 
 	def __init__(self):
-		#plt.style.use("seaborn-deep")
 		self.columns = []
-		self.db = None
-		self.embedding = 32
 		self.epochs = 5
-		self.hub = "https://tfhub.dev/google/nnlm-en-dim50/2"
 		self.labels_test = {}
 		self.labels_train = {}
+		self.locations = {}
 		self.maxlen = 150
 		self.model = None
 		self.oov = "<OOV>"
@@ -34,7 +30,7 @@ class Classifier():
 		self.padding = "post"
 		self.test = []
 		self.train = []
-		self.training_size = 10000
+		self.training_size = 20000
 		self.types = {}
 		self.vocab_size = 10000
 		self.__loadDicts__()
@@ -43,7 +39,10 @@ class Classifier():
 	def __loadDicts__(self):
 		# Loads types dict
 		for i in readFile(ENCODING, header = False, d = ","):
-			self.types[int(i[1])] = i[0]
+			if i[0] == "Type":
+				self.types[int(i[2])] = i[1]
+			else:
+				self.locations[int(i[2])] = i[1]
 
 	def __getTokenizer__(self, df):
 		# Tokenizes training and testing data
@@ -54,11 +53,9 @@ class Classifier():
 		df.pop("Necropsy")
 		self.columns = list(df.columns)
 		for i in self.columns:
-			#df[[i]] = df[[i]].astype(np.int32)
 			col = np.asarray(df.pop(i)).astype(np.int32)
 			self.labels_train[i] = col[:self.training_size].reshape((-1,1))
 			self.labels_test[i] = col[self.training_size:].reshape((-1,1))
-		#self.labels_train, self.labels_test = df[:self.training_size], df[self.training_size:]
 		tokenizer = Tokenizer(num_words = self.vocab_size, oov_token = self.oov)
 		tokenizer.fit_on_texts(values)
 		self.train = np.array(pad_sequences(tokenizer.texts_to_sequences(train), maxlen = self.maxlen, padding = self.padding, truncating = self.padding))
@@ -68,8 +65,7 @@ class Classifier():
 		# Reads dataframe and splits into training and testing datasets
 		print("\n\tReading input file...")
 		df = pd.read_csv(INFILE, delimiter = ",")
-		# Randomly shuffle dataframe
-		df = df.sample(frac = 1).reset_index(drop = True)
+		#self.training_size = int(len(df) / 2)
 		self.__getTokenizer__(df)
 
 #-----------------------------------------------------------------------------
@@ -87,8 +83,8 @@ class Classifier():
 			labels.extend([name, val])
 		# Reduce plot size so legend is not covering it
 		plt.tight_layout(rect=[0,0,0.65,0.65])
-		plt.legend(labels, loc='center left', bbox_to_anchor=(1, 0.5))
-		plt.savefig("{}/{}.svg".format(self.outdir, metric), format="svg")
+		plt.legend(labels, loc = 'center left', bbox_to_anchor = (1, 0.5))
+		plt.savefig("{}/{}.svg".format(self.outdir, metric), format = "svg")
 		# Clear plot
 		plt.clf()
 
@@ -97,11 +93,13 @@ class Classifier():
 		return tf.keras.layers.Dense(units = 1, activation = "sigmoid", name = name)(input_layer)
 
 	def __typeLayer__(self, input_layer):
-		# Returns output layer for types
-		dense1 = tf.keras.layers.Dense(units = 128, activation = "relu")(input_layer)
-		dense2 = tf.keras.layers.Dense(units = 256, activation = "relu")(dense1)
-		flattened = tf.keras.layers.Flatten()(dense2)
-		return tf.keras.layers.Dense(units = len(self.types.keys()), activation = "softmax", name = "Type")(flattened)
+		# Returns output layer for locations and types
+		'''dense1 = tf.keras.layers.Dense(units = 128, activation = "relu")(input_layer)
+		dense2 = tf.keras.layers.Dense(units = 64, activation = "relu")(dense1)
+		flattened = tf.keras.layers.Flatten()(dense2)'''
+		loc = tf.keras.layers.Dense(units = len(self.locations.keys()), activation = "softmax", name = "Location")(input_layer)
+		typ = tf.keras.layers.Dense(units = len(self.types.keys()), activation = "softmax", name = "Type")(input_layer)
+		return loc, typ
 
 	def __multiOutputModel__(self):
 		# Defines multiple-output model
@@ -116,10 +114,11 @@ class Classifier():
 		dense1 = tf.keras.layers.Dense(units = 64, activation = "relu")(bidirectional)
 		dense2 = tf.keras.layers.Dense(units = 32, activation = "relu")(dense1)
 		dense3 = tf.keras.layers.Dense(units = 16, activation = "relu")(dense2)
-		flattened = tf.keras.layers.Flatten()(dense3)
-		for i in self.columns[:-1]:
+		dense4 = tf.keras.layers.Dense(units = 8, activation = "relu")(dense3)
+		flattened = tf.keras.layers.Flatten()(dense4)
+		for i in self.columns[:-2]:
 			outputs.append(self.__outputLayer__(i, flattened))
-		outputs.append(self.__typeLayer__(bidirectional))
+		outputs.extend(self.__typeLayer__(flattened))
 		# Define the model with the input layer and a list of output layers
 		return tf.keras.Model(inputs = input_layer, outputs = outputs, name = self.outdir)
 
@@ -127,7 +126,7 @@ class Classifier():
 		# Returns loss estimation for each output column
 		ret = {}
 		for i in self.columns:
-			if i == "Type":
+			if i == "Type" or i == "Location":
 				ret[i] = tf.keras.losses.SparseCategoricalCrossentropy()
 			else:
 				ret[i] = tf.keras.losses.BinaryCrossentropy()
@@ -137,8 +136,8 @@ class Classifier():
 		# Trains species name classifier
 		print("\tTraining model...")
 		self.model = self.__multiOutputModel__()
-		tf.keras.utils.plot_model(self.model, "{}/model_plot.png".format(self.outdir), show_shapes=True)
-		self.model.compile(loss = self.__getLoss__(), optimizer = 'adam', metrics = ['accuracy'])
+		tf.keras.utils.plot_model(self.model, "{}/model_plot.png".format(self.outdir), show_shapes = True)
+		self.model.compile(loss = self.__getLoss__(), optimizer = 'adam', metrics = ["accuracy"])
 		print(self.model.summary())
 		history = self.model.fit(self.train, self.labels_train,
 			epochs = self.epochs, 
@@ -156,7 +155,7 @@ class Classifier():
 
 def main():
 	start = datetime.now()
-	parser = ArgumentParser("")
+	parser = ArgumentParser("Defines TensorFlow model for comparative oncology record diagnosis.")
 	parser.add_argument("-i", help = "Path to unformatted training data. Pre-formats the data only. Run again without infile argument to train the model.")
 	args = parser.parse_args()
 	if args.i:
