1a6ffc8410e30eec59968d794f0df990f8377463
icwells <smrupp16@gmail.com>
2022-03-02 15:47:18 -0700

    Skips multiple hits

--- scripts/updateDiagnoses/main.go
+++ scripts/updateDiagnoses/main.go
@@ -100,12 +100,12 @@ func (l *lzDiagnosis) updateCell(column, id, val string) {
 
 func (l *lzDiagnosis) getComments(i *dataframe.Series) string {
 	// Returns relevant comments for parsing
-	ret, _ := i.GetCell(l.col.Comments)
+	ret, _ := i.GetCell(l.col.comments)
 	if service, _ := i.GetCell(l.col.service); service == "NWZP" {
 		for idx, i := range ret {
 			if unicode.IsLetter(i) && !unicode.IsUpper(i) {
 				if idx >= 4 {
-					return ret[:idx]
+					return ret[:idx - 1]
 				} else {
 					break
 				}
@@ -122,50 +122,51 @@ func (l *lzDiagnosis) checkRecord(i *dataframe.Series) bool {
 	neoplasia := 1
 	if typ, _ := i.GetCell(l.col.typ); !strings.Contains(typ, ";") {
 		comments := l.getComments(i)
-		hyp, _ := i.GetCellInt(l.col.hyperplasia)
-		loc, _ := i.GetCell(l.col.location)
-		mal, _ := i.GetCell(l.col.malignant)
-		mp, _ := i.GetCellInt(l.col.masspresent)
 		sex, _ := i.GetCell(l.col.sex)
-		tis, _ := i.GetCell(l.col.tissue)
-		tumor, tissue, location, malignant := l.match.GetTumor(comments, sex, true)
-		if tumor != "NA" && !strings.Contains(tumor, ";") {
-			if tumor == "hyperplasia" {
-				hyperplasia = 1
-				neoplasia = 0
-				l.hyperplasia++
-			}
-			if tumor != typ {
-				l.updateCell(l.col.typ, i.Name, tumor)
-				ret = true
-			}
-			if hyp != hyperplasia {
-				l.updateCell(l.col.hyperplasia, i.Name, strconv.Itoa(hyperplasia))
-				ret = true
-			}
-			if mp != neoplasia {
-				l.updateCell(l.col.masspresent, i.Name, strconv.Itoa(neoplasia))
-				ret = true
-				if mp == 0 {
-					l.neoplasia++
+		if tumor, tissue, location, malignant := l.match.GetTumor(comments, sex, true); !strings.Contains(tumor, ";") {
+			hyp, _ := i.GetCellInt(l.col.hyperplasia)
+			loc, _ := i.GetCell(l.col.location)
+			mal, _ := i.GetCell(l.col.malignant)
+			mp, _ := i.GetCellInt(l.col.masspresent)
+			tis, _ := i.GetCell(l.col.tissue)
+			if tumor != "NA" {
+				if tumor == "hyperplasia" {
+					hyperplasia = 1
+					neoplasia = 0
+					l.hyperplasia++
+				}
+				if tumor != typ {
+					l.updateCell(l.col.typ, i.Name, tumor)
+					ret = true
+				}
+				if hyp != hyperplasia {
+					l.updateCell(l.col.hyperplasia, i.Name, strconv.Itoa(hyperplasia))
+					ret = true
+				}
+				if mp != neoplasia {
+					l.updateCell(l.col.masspresent, i.Name, strconv.Itoa(neoplasia))
+					ret = true
+					if mp == 0 {
+						l.neoplasia++
+					}
 				}
 			}
-		}
-		if tissue != "NA" && tissue != tis {
-			l.updateCell(l.col.tissue, i.Name, tissue)
-			ret = true
-		}
-		if location != "NA" && location != loc {
-			l.updateCell(l.col.location, i.Name, location)
-			ret = true
-		}
-		if malignant != "-1" && malignant != mal {
-			l.updateCell(l.col.malignant, i.Name, malignant)
-			ret = true
-		}
-		if ret {
-			row := []string{comments, strconv.Itoa(mp), strconv.Itoa(neoplasia), strconv.Itoa(hyp), strconv.Itoa(hyperplasia), typ, tumor, tis, tissue, loc, location}
-			l.summary = append(l.summary, row)
+			if tissue != "NA" && tissue != tis {
+				l.updateCell(l.col.tissue, i.Name, tissue)
+				ret = true
+			}
+			if location != "NA" && location != loc {
+				l.updateCell(l.col.location, i.Name, location)
+				ret = true
+			}
+			if malignant != "-1" && malignant != mal {
+				l.updateCell(l.col.malignant, i.Name, malignant)
+				ret = true
+			}
+			if ret {
+				row := []string{comments, strconv.Itoa(mp), strconv.Itoa(neoplasia), strconv.Itoa(hyp), strconv.Itoa(hyperplasia), typ, tumor, tis, tissue, loc, location}
+				l.summary = append(l.summary, row)
+			}
 		}
 	}
 	return ret
@@ -174,7 +175,7 @@ func (l *lzDiagnosis) checkRecord(i *dataframe.Series) bool {
 func (l *lzDiagnosis) checkRecords() {
 	// Updates life history table with converted values
 	var count, updated int
-	l.logger.Println("Updating London Zoo records...")
+	l.logger.Println("Comparing diagnoses...")
 	for i := range l.diag.Iterate() {
 		count++
 		if l.checkRecord(i) {
